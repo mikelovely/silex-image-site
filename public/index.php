450d620b8fcc3c22cdc19d95bc1005e6235d8e94
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-08-28 14:23:16 +0100

    require in twig for templating and create a basic view

--- public/index.php
+++ public/index.php
@@ -3,3 +3,15 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application;
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider, [
+    'twig.path' => __DIR__ . '/../views'
+]);
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('home.twig');
+});
+
+$app->run();
