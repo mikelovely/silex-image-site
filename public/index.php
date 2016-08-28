ea42f0e754d469aa74e6f630386cfe053a46079b
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-08-28 16:58:57 +0100

    add doctrine dbal for PDOing

--- public/index.php
+++ public/index.php
@@ -10,7 +10,26 @@ $app->register(new Silex\Provider\TwigServiceProvider, [
     'twig.path' => __DIR__ . '/../views'
 ]);
 
+$app->register(new Silex\Provider\DoctrineServiceProvider, [
+    'db.options' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'awesome-image',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ]
+]);
+
 $app->get('/', function () use ($app) {
+    
+    $images = $app['db']->prepare("SELECT * FROM images");
+    $images->execute();
+    $images = $images->fetchAll(\PDO::FETCH_CLASS, \AI\Models\Image::class);
+
+    var_dump($images);
+    die();
+
     return $app['twig']->render('home.twig');
 });
 
