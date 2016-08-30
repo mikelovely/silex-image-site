a8463a2b3e501ae2d04694e2b6b745a60c6077a8
Mike Lovely <mike-lovely@hotmail.co.uk>
2016-08-30 19:02:58 +0100

    create post method to upload files to uploadcare and add record to database

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application;
@@ -30,6 +32,26 @@ $app->get('/', function () use ($app) {
     $images = $images->fetchAll(\PDO::FETCH_CLASS, \AI\Models\Image::class);
 
     return $app['twig']->render('home.twig');
-});
+})->bind('home');
+
+$app->post('/upload', function (Request $request) use ($app) {
+    if ($request->get('file_id') === '') {
+        return $app->redirect($app['url_generator']->generate('home'));
+    }
+
+    $file = $app['uploadcare']->getFile($request->get('file_id'));
+
+    $store = $app['db']->prepare("
+        INSERT INTO images (hash, url, created_at)
+        VALUES (:hash, :url, NOW())
+    ");
+
+    $store->execute([
+        'hash' => bin2hex(random_bytes(20)),
+        'url' => $file->getUrl(),
+    ]);
+
+    return $app->redirect($app['url_generator']->generate('home'));
+})->bind('image.upload');
 
 $app->run();
